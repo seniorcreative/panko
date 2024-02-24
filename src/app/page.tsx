84d1b93f637c0c6fd0e66da3f04b2dcee037989d
Steven Smith <stesmi@gmail.com>
2024-02-25 07:46:46 +1100

    Add more logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,33 @@ import Buildxact from "../../public/Buildxact-Logo-SVG.svg";
 import Telstra from "../../public/telstra.png";
 import Worksafe from "../../public/WorkSafe-logo-BW-grey-approved-PNG-2017.png";
 import ASM from "../../public/ASM-White-on-black.png";
+import BMW from "../../public/BMW-Logo.png";
+import Sportsyear from "../../public/Sportsyear-logo-lod@2x.png";
+import Mini from "../../public/mini-logo-06.jpg";
+import RevitCourse from "../../public/autodesk-revit-course-logo@2x.png";
 
-export type LogoObj = { t: string; i: StaticImageData };
+export type LogoObj = { t: string; i: StaticImageData; c: string | undefined };
 
 export default function Home() {
   const Logos: LogoObj[] = [
-    { t: "Nine Network: Graphics", i: NineLogo },
-    { t: "BankFirst: Finance Application", i: BankFirst },
-    { t: "Buildxact: Building Project Estimation", i: Buildxact },
-    { t: "Telstra: Resume Generator", i: Telstra },
-    { t: "Australian Sports Museum: Touchscreen", i: ASM },
-    { t: "Worksafe: Pilot App", i: Worksafe },
+    { t: "Nine Network: Graphics", i: NineLogo, c: undefined },
+    { t: "BankFirst: Finance Application", i: BankFirst, c: undefined },
+    { t: "Buildxact: Building Project Estimation", i: Buildxact, c: undefined },
+    { t: "Telstra: Resume Generator", i: Telstra, c: undefined },
+    { t: "Australian Sports Museum: Touchscreen", i: ASM, c: undefined },
+    { t: "Worksafe: Pilot App", i: Worksafe, c: undefined },
+    { t: "BMW: Finance Application", i: BMW, c: "invert grayscale" },
+    {
+      t: "Sportsyear: E-Commerce",
+      i: Sportsyear,
+      c: "invert scale-50 grayscale opacity-60",
+    },
+    { t: "Mini: Asia Pac Site Dev for Monkii", i: Mini, c: undefined },
+    {
+      t: "RevitCourse: Booking System",
+      i: RevitCourse,
+      c: "invert grayscale opacity-60 scale-75",
+    },
   ];
 
   useEffect(() => {
@@ -57,7 +73,7 @@ export default function Home() {
             <div
               key={logo.t}
               title={logo.t}
-              className="grayscale opacity-70"
+              className={logo.c || "grayscale opacity-60"}
               style={{
                 backgroundImage: `url(${logo.i.src})`,
                 backgroundSize: "contain",
