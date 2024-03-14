958015c162ecbffc7ca3ac90d36b748490db9ac2
Steven Smith <stesmi@gmail.com>
2024-03-14 21:14:49 +1100

    Add folio section draft

--- src/app/folio/[slug]/page.tsx
+++ src/app/folio/[slug]/page.tsx
@@ -1,13 +1,100 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import React from "react";
+
+const posts = [
+  {
+    id: 1,
+    title: "Carlton Natural Blonde",
+    href: "#",
+    description:
+      "Retro galaga with original sound effect. Great times. Took 6 days to code.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Marketing", href: "#" },
+    author: {
+      name: "Panko Digital",
+      role: "Marketing Website Game",
+      href: "#",
+      imageUrl: "/work/carltonnaturalblonde~4~4.jpg",
+    },
+  },
+  // More posts...
+];
 
 export default function Page() {
   const pathname = usePathname();
+
   return (
-    <section className="mx-auto container my-24">
-      <p>Category: {decodeURI(pathname)}</p>
-    </section>
+    <div className="py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {decodeURI(pathname)}
+          </h2>
+          {/* <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p> */}
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article
+              key={post.id}
+              className="flex max-w-xl flex-col items-start justify-between"
+            >
+              <Image
+                src={post.author.imageUrl}
+                alt=""
+                width={350}
+                height={224}
+                className="rounded-2xl"
+              />
+              <div className="flex items-center gap-x-4 text-xs">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.category.href}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  {post.description}
+                </p>
+              </div>
+              <div className="relative mt-8 flex items-center gap-x-4">
+                <Image
+                  src={post.author.imageUrl}
+                  alt=""
+                  width={20}
+                  height={20}
+                  className="h-10 w-10 rounded-full bg-gray-50"
+                />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
