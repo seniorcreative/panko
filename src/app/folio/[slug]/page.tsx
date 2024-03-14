4924668ad52016b55b9bb90386f328819baf6f1a
Steven Smith <stesmi@gmail.com>
2024-03-14 21:34:18 +1100

    Folio page rough

--- src/app/folio/[slug]/page.tsx
+++ src/app/folio/[slug]/page.tsx
@@ -11,14 +11,16 @@ const posts = [
     href: "#",
     description:
       "Retro galaga with original sound effect. Great times. Took 6 days to code.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
+    date: "2010",
     category: { title: "Marketing", href: "#" },
     author: {
       name: "Panko Digital",
       role: "Marketing Website Game",
       href: "#",
-      imageUrl: "/work/carltonnaturalblonde~4~4.jpg",
+      imageUrl: [
+        "/work/carltonnaturalblonde~4~1.jpg",
+        "/work/carltonnaturalblonde~4~4.jpg",
+      ],
     },
   },
   // More posts...
@@ -34,63 +36,51 @@ export default function Page() {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             {decodeURI(pathname)}
           </h2>
-          {/* <p className="mt-2 text-lg leading-8 text-gray-600">
+          {/* <p className="mt-2 text-lg leading-8 text-slate-900">
             Learn how to grow your business with our expert advice.
           </p> */}
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-10 border-t border-gray-200 pt-10">
           {posts.map((post) => (
-            <article
-              key={post.id}
-              className="flex max-w-xl flex-col items-start justify-between"
-            >
-              <Image
-                src={post.author.imageUrl}
-                alt=""
-                width={350}
-                height={224}
-                className="rounded-2xl"
-              />
-              <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-500">
-                  {post.date}
-                </time>
-                <a
-                  href={post.category.href}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {post.category.title}
-                </a>
+            <article key={post.id} className="">
+              <div className="grid grid-cols-2 gap-4">
+                {post.author.imageUrl.map((img) => (
+                  <div key={img}>
+                    <Image
+                      src={img}
+                      alt=""
+                      width={700}
+                      height={448}
+                      className="rounded-2xl mb-4"
+                    />
+                  </div>
+                ))}
               </div>
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={post.href}>
-                    <span className="absolute inset-0" />
-                    {post.title}
+
+              <section className="w-1/2">
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime={post.date} className="text-white">
+                    {post.date}
+                  </time>
+                  <a
+                    href={post.category.href}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-slate-900 hover:bg-gray-100"
+                  >
+                    {post.category.title}
                   </a>
-                </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                  {post.description}
-                </p>
-              </div>
-              <div className="relative mt-8 flex items-center gap-x-4">
-                <Image
-                  src={post.author.imageUrl}
-                  alt=""
-                  width={20}
-                  height={20}
-                  className="h-10 w-10 rounded-full bg-gray-50"
-                />
-                <div className="text-sm leading-6">
-                  <p className="font-semibold text-gray-900">
-                    <a href={post.author.href}>
-                      <span className="absolute inset-0" />
-                      {post.author.name}
-                    </a>
-                  </p>
-                  <p className="text-gray-600">{post.author.role}</p>
                 </div>
-              </div>
+                <div className="group relative">
+                  <h3 className="mt-3 text-xl font-semibold leading-6 text-gray-900 group-hover:text-slate-900">
+                    <a href={post.href}>
+                      <span className="absolute inset-0" />
+                      {post.title}
+                    </a>
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-slate-900">
+                    {post.description}
+                  </p>
+                </div>
+              </section>
             </article>
           ))}
         </div>
