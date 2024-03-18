b5036f76f40929314d0f44ce0fd77e6474b99aeb
Steven Smith <stesmi@gmail.com>
2024-03-19 08:05:26 +1100

    Better folio layout

--- src/app/folio/[slug]/page.tsx
+++ src/app/folio/[slug]/page.tsx
@@ -10,12 +10,12 @@ const posts = [
     title: "Carlton Natural Blonde",
     href: "#",
     description:
-      "Retro galaga with original sound effect. Great times. Took 6 days to code.",
+      "Retro Galaga. I can remember having a wicked time smashing out the code for this in 6 days. It needed to be embedded into a larger marketing site. I was able to find all the original SFX online. It was not the full game and was not meant to be but it was a lot of fun and I later hosted it seperately with it's own scoreboard API.",
     date: "2010",
     category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Panko Digital",
-      role: "Marketing Website Game",
+    meta: {
+      client: "Emote Digital",
+      category: "Marketing Agency",
       href: "#",
       imageUrl: [
         "/work/carltonnaturalblonde~4~1.jpg",
@@ -31,20 +31,17 @@ export default function Page() {
 
   return (
     <div className="py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
+      <div className="bg-white mx-auto max-w-7xl p-6 lg:p-8">
+        <div className="max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             {decodeURI(pathname)}
           </h2>
-          {/* <p className="mt-2 text-lg leading-8 text-slate-900">
-            Learn how to grow your business with our expert advice.
-          </p> */}
         </div>
-        <div className="mx-auto mt-10 border-t border-gray-200 pt-10">
+        <div className="mt-10 border-t border-gray-200 pt-10">
           {posts.map((post) => (
             <article key={post.id} className="">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                {post.author.imageUrl.map((img) => (
+                {post.meta.imageUrl.map((img) => (
                   <div key={img}>
                     <Image
                       src={img}
@@ -57,28 +54,22 @@ export default function Page() {
                 ))}
               </div>
 
-              <section className="w-1/2">
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.date} className="text-white">
+              <section className="w-full">
+                <div className="group flex justify-between align-middle">
+                  <h3 className="text-xl font-semibold leading-6 white group-hover:text-slate-900">
+                    {post.title}
+                  </h3>
+                  <time dateTime={post.date} className="text-slate-400">
                     {post.date}
                   </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-slate-900 hover:bg-gray-100"
-                  >
-                    {post.category.title}
-                  </a>
                 </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-xl font-semibold leading-6 text-gray-900 group-hover:text-slate-900">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-slate-900">
-                    {post.description}
-                  </p>
+                <p className="w-3/4 mt-5 line-clamp-8 text-sm leading-6 text-slate-900">
+                  {post.description}
+                </p>
+                <div className="w-full flex justify-end">
+                  <small className="rounded-full bg-slate-300 px-3 py-1.5 text-slate-900">
+                    {post.meta.client}
+                  </small>
                 </div>
               </section>
             </article>
