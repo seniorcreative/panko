2b6f51f2399f65104552f8d8c1631f6c482f3472
Steven Smith <stesmi@gmail.com>
2024-04-08 16:41:28 +1000

    Carousel wiring and content clean up

--- src/app/components/carouselreact.tsx
+++ src/app/components/carouselreact.tsx
@@ -4,24 +4,17 @@ import { Carousel } from "flowbite-react";
 import Image from "next/image";
 import React from "react";
 
-export default function CarouselReactComponent() {
+export default function CarouselReactComponent({
+  images,
+}: {
+  images: string[];
+}) {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel slideInterval={5000}>
-        <Image
-          key="img1"
-          src="/work/flux~4~1.jpg"
-          width="1024"
-          height="512"
-          alt="..."
-        />
-        <Image
-          key="img2"
-          src="/work/flux~4~2.jpg"
-          width="1024"
-          height="512"
-          alt="..."
-        />
+      <Carousel slideInterval={4000 + Math.random() * 2000}>
+        {images.map((image: string) => (
+          <Image key={image} src={image} width="1024" height="512" alt="..." />
+        ))}
       </Carousel>
     </div>
   );
--- src/app/folio/[slug]/page.tsx
+++ src/app/folio/[slug]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Image from "next/image";
 import React from "react";
 
 import CarouselReactComponent from "../../components/carouselreact";
@@ -12,45 +11,28 @@ export default function Page() {
 
   return (
     <div className="mt-24 mx-auto py-6 sm:py-6">
-      <div className="max-w-2xl lg:mx-0">
+      {/* <div className="max-w-2xl lg:mx-0">
         <h2 className="text-xl font-bold sm:ps-2 tracking-tight text-white sm:text-xl">
           {decodeURI(pathname).toLowerCase()}
         </h2>
-      </div>
+      </div> */}
 
-      {content.en.workSingle.map((post: any) => (
+      {content.en.work.map((post: any) => (
         <article key={post.id} className="mb-4 p-4">
-          <div className="flex w-full justify-between">
-            <h3 className="text-xl font-semibold leading-6 text-slate-900">
-              {post.title}
-            </h3>
-            <div className="text-end text-lg">
-              <small className="px-3 text-slate-900">{post.meta.client}</small>
-              <time dateTime={post.date} className="text-xs text-white">
-                {post.date}
-              </time>
-            </div>
+          <h3 className="text-xl font-semibold leading-6 text-slate-900">
+            {post.title}
+          </h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+            <CarouselReactComponent images={post.meta.imageUrl} />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <CarouselReactComponent />
-            {/* {post.meta.imageUrl.map((img: any) => (
-              <div key={img} className="flex justify-center">
-                <Image
-                  src={img}
-                  alt=""
-                  width={640}
-                  height={320}
-                  className="rounded-2xl mb-4"
-                />
-              </div>
-            ))} */}
-          </div>
-
-          <section className="w-full">
-            <p className="md:w-3/4 mt-2 line-clamp-2 text-lg leading-6 text-slate-900">
+          <section className="w-full mb-4">
+            <p className="md:w-3/4 mt-2 line-clamp-5 text-sm leading-6 text-slate-900">
               {post.description}
             </p>
           </section>
+          <small className="text-slate-900 text-sm">
+            <strong>Client</strong> {post.meta.client}
+          </small>
         </article>
       ))}
     </div>
