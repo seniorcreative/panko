7fbc8f6e0a69b7d6faa8dddbafe09d80f74337f3
Steven Smith <stesmi@gmail.com>
2024-04-20 08:53:48 +1000

    Folio carousels improvements

--- src/app/components/carouselreact.tsx
+++ src/app/components/carouselreact.tsx
@@ -10,10 +10,10 @@ export default function CarouselReactComponent({
   images: string[];
 }) {
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <div className="h-80 sm:h-80 xl:h-96">
       <Carousel slideInterval={4000 + Math.random() * 2000}>
         {images.map((image: string) => (
-          <Image key={image} src={image} width="1024" height="512" alt="..." />
+          <Image key={image} src={image} width="512" height="512" alt="..." />
         ))}
       </Carousel>
     </div>
--- src/app/folio/[slug]/page.tsx
+++ src/app/folio/[slug]/page.tsx
@@ -19,20 +19,24 @@ export default function Page() {
 
       {content.en.work.map((post: any) => (
         <article key={post.id} className="mb-4 p-4">
-          <h3 className="text-xl font-semibold leading-6 text-slate-900">
-            {post.title}
-          </h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
-            <CarouselReactComponent images={post.meta.imageUrl} />
+          <div className="grid grid-cols-6 md:grid-cols-6 gap-4 my-4 align-middle">
+            <div className="col-start-2 col-span-4">
+              <h3 className="text-xl font-semibold leading-6 text-center text-slate-900">
+                {post.title}
+              </h3>
+              <div className="my-4">
+                <CarouselReactComponent images={post.meta.imageUrl} />
+              </div>
+              <section className="w-full mb-4">
+                <p className="mt-2 line-clamp-5 text-sm leading-6 text-slate-900">
+                  {post.description}
+                </p>
+              </section>
+              <small className="text-slate-900 text-sm">
+                <strong>Client</strong> {post.meta.client}
+              </small>
+            </div>
           </div>
-          <section className="w-full mb-4">
-            <p className="md:w-3/4 mt-2 line-clamp-5 text-sm leading-6 text-slate-900">
-              {post.description}
-            </p>
-          </section>
-          <small className="text-slate-900 text-sm">
-            <strong>Client</strong> {post.meta.client}
-          </small>
         </article>
       ))}
     </div>
