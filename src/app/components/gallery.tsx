d31617f6cfdc10a6c74ea6b40731f0db8d48f9d8
Steven Smith <stesmi@gmail.com>
2024-04-26 08:36:19 +1000

    Catch up

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -3,17 +3,49 @@
 import Image from "next/image";
 import React from "react";
 
-export default function Gallery({ images }: { images: string[] }) {
+export default function Gallery({
+  images,
+  description,
+}: {
+  images: string[];
+  description: string;
+}) {
   return (
-    <section className="relative p-10 overflow-auto">
+    <section
+      className="relative mb-12"
+      style={{ height: "480px", paddingTop: "80px", paddingBottom: "80px" }}
+    >
       <div
-        className="h-96 blur-lg opacity-80 flex-grow flex"
-        style={{ backgroundImage: `url(${images[0]})` }}
+        className="opacity-60 blur-md bl w-full flex absolute"
+        style={{
+          backgroundImage: `url(${images[0]})`,
+          height: "480px",
+          top: "0",
+        }}
       ></div>
-      <div className="flex absolute top-10">
+      <div
+        className="absolute flex overflow-x-auto"
+        style={{ height: "320px", top: "80px", left: "20px" }}
+      >
+        <div
+          className="inline-flex flex-nowrap ms-2 first:ms-0 bg-white p-4"
+          style={{ minWidth: "320px", width: "320px", height: "320px" }}
+        >
+          {description}
+        </div>
         {images.map((image: string) => (
-          <div key={image} className="flex-1 me-2" style={{ width: "512px" }}>
-            <Image src={image} width="512" height="512" alt="..." />
+          <div
+            key={image}
+            className="inline-flex flex-nowrap ms-2 first:ms-0"
+            style={{ height: "320px" }}
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={image}
+              style={{ width: "auto", height: "320px" }}
+              className="object-cover"
+              alt="..."
+            />
           </div>
         ))}
       </div>
--- src/app/folio/[slug]/page.tsx
+++ src/app/folio/[slug]/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   const pathname = usePathname();
 
   return (
-    <div className="mt-24 mx-auto py-6 sm:py-6">
+    <div className="mt-24 mx-auto py-6 sm:py-6 w-full">
       {/* <div className="max-w-2xl lg:mx-0">
         <h2 className="text-xl font-bold sm:ps-2 tracking-tight text-white sm:text-xl">
           {decodeURI(pathname).toLowerCase()}
@@ -19,40 +19,38 @@ export default function Page() {
       </div> */}
 
       {content.en.work.map((post: any) => (
-        <article key={post.id} className="mb-4 p-4">
-          <div className="my-4 align-middle">
-            <div className="">
-              <h3 className="text-xl font-semibold leading-6 text-slate-900">
-                {post.title}
-              </h3>
-              {post.href && (
-                <div className="w-full my-2">
-                  Launch this
-                  <a
-                    href={`/${post.href}`}
-                    role="Button"
-                    className="ps-2 text underline"
-                    title={`View ${post.title} in a new window`}
-                  >
-                    {post.href}
-                  </a>
-                </div>
-              )}
-            </div>
+        <article key={post.id} className="w-full">
+          <div className="my-4 ps-4 align-middle">
+            <h3 className="text-xl font-semibold leading-6 text-slate-900">
+              {post.title}
+            </h3>
+            <small className="text-slate-900 text-sm ps-2 border-s-2 border-slate-300">
+              For&nbsp;<strong>{post.meta.client}</strong>
+            </small>
+            {post.href && (
+              <div className="w-full my-2">
+                Launch this
+                <a
+                  href={`/${post.href}`}
+                  role="Button"
+                  className="ps-2 text underline"
+                  title={`View ${post.title} in a new window`}
+                >
+                  {post.href}
+                </a>
+              </div>
+            )}
           </div>
 
-          <Gallery images={post.meta.imageUrl} />
+          <Gallery images={post.meta.imageUrl} description={post.description} />
 
-          <div className="grid grid-cols-3 gap-4 align-middle">
-            <section className="w-full mt-8 col-span-3 md:col-span-2">
-              <p className="m-4 text-md leading-6 border-t-2 border-slate-300 pt-4  text-slate-900">
+          {/* <div className="grid grid-cols-3 gap-4 align-middle">
+            <section className="w-full col-span-3 md:col-span-2">
+              <p className="m-4 text-md leading-6 text-slate-900">
                 {post.description}
               </p>
-              <small className="text-slate-900 text-sm ms-4 ps-4 border-s-2 border-slate-300">
-                For&nbsp;<strong>{post.meta.client}</strong>
-              </small>
             </section>
-          </div>
+          </div> */}
         </article>
       ))}
     </div>
