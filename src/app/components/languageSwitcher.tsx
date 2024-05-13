f501ffdac963942a8ae140e2d99acdf1c9c21ac7
Steven Smith <stesmi@gmail.com>
2024-05-14 08:29:29 +1000

    Next Js language switcher basikke

--- src/app/components/languageSwitcher.tsx
+++ src/app/components/languageSwitcher.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use server";
 
 import React from "react";
 import { cookies } from "next/headers";
@@ -6,39 +6,31 @@ import Link from "next/link";
 
 import { setDefaultLocaleCookie, changeLanguageCookieValue } from "../actions";
 
+import LanguageSwitcherIdiom from "./languageSwitcherIdiom";
+
 const locales = ["en-US", "zh-CN", "fr", "es"];
 
-export default function LanguageSwitcher() {
+export default async function LanguageSwitcher() {
   //   setDefaultLocaleCookie();
 
-  const currentLocaleFromCookie = "en-US"; // cookies().get("NEXT_LOCALE")?.value;
+  const currentLocaleFromCookie = cookies().get("NEXT_LOCALE")?.value as string;
+
+  const languageMap = [
+    { lang: "en-US", icon: "language_us" },
+    { lang: "zh-CN", icon: "language_chinese_quick" },
+  ];
 
   return (
     <>
-      <a
-        className={`text-gray-800 text-sm ${
-          currentLocaleFromCookie === "en-US" ? "underline" : ""
-        }`}
-        href="#"
-        onClick={() => {
-          changeLanguageCookieValue("en-US");
-        }}
-      >
-        <span className="material-symbols-outlined">language_us</span>
-      </a>
-      <a
-        className={`text-gray-800 text-sm ${
-          currentLocaleFromCookie === "en-US" ? "underline" : ""
-        }`}
-        href="#"
-        onClick={() => {
-          changeLanguageCookieValue("zh-CN");
-        }}
-      >
-        <span className="material-symbols-outlined">
-          language_chinese_quick
-        </span>
-      </a>
+      {languageMap.map(({ lang, icon }) => (
+        <LanguageSwitcherIdiom
+          key={lang}
+          language={lang}
+          currentLanguage={currentLocaleFromCookie}
+          icon={icon}
+          changeMethod={changeLanguageCookieValue}
+        />
+      ))}
     </>
   );
 }
---
+++ src/app/components/languageSwitcherIdiom.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+
+type languageButtonType = {
+  language: string;
+  icon: string;
+  currentLanguage: string;
+  changeMethod: (language: string) => {};
+};
+
+export default function LanguageSwitcherIdiom({
+  language,
+  icon,
+  currentLanguage,
+  changeMethod,
+}: languageButtonType) {
+  return (
+    <a
+      className={`text-gray-800 text-sm mx-1 ${
+        currentLanguage === language ? "bg-black text-white" : ""
+      }`}
+      href="#"
+      onClick={() => {
+        changeMethod(language);
+      }}
+    >
+      <span className="material-symbols-outlined">{icon}</span>
+    </a>
+  );
+}
