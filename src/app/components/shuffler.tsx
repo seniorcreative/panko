f310bdee7ca55022fa511eb29dea29fa82f472da
Steven Smith <stesmi@gmail.com>
2024-05-22 17:57:04 +1000

    Shuffler with useEffect

--- src/app/components/shuffler.tsx
+++ src/app/components/shuffler.tsx
@@ -1,98 +1,108 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import anime from "animejs";
 import React, { useRef } from "react";
 
 export default function Shuffler() {
-  const words = [
-    "Precision App Nouse Knockout Offerings",
-    "Press A New Key Object",
-    "Potential Activated Nurturing Knowhow Openly",
-    "Philosopher And Naturally Knowledgeable Ordinator",
-    "Pushing Against Normal Known Obstacles",
-    "Processing Actual Numbers Keeping Open-minded",
-    "Programming Advanced Nominal Keeping Open",
-    "Pursuing Adaptable New Knowledge Output",
-    "Providing A Nuanced Kind Organisation",
-  ];
+  const [letters, setLetters] = useState([]);
+  const [keyMap, setKeyMap] = useState([]);
+  const [shuffles, setShuffles] = useState(0);
 
   useEffect(() => {
+    const words = [
+      "Precision App Nouse Knockout Offerings",
+      "Press A New Key Object",
+      "Potential Activated Nurturing Knowhow Openly",
+      "Philosopher And Naturally Knowledgeable Ordinator",
+      "Pushing Against Normal Known Obstacles",
+      "Processing Actual Numbers Keeping Open-minded",
+      "Programming Advanced Nominal Keeping Open",
+      "Pursuing Adaptable New Knowledge Output",
+      "Providing A Nuanced Kind Organisation",
+    ];
+
+    const getRandomWord = () => {
+      const wordArrayLen = words.length;
+      const wordIndex = Math.round(Math.random() * wordArrayLen) - 1;
+      console.log(wordArrayLen, wordIndex);
+      const wordSelection = words[wordIndex];
+      if (wordSelection) {
+        const ws: string[] = wordSelection.split(" ");
+        const keys = ["p", "a", "n", "k", "o"];
+        return { letters: ws, keyMap: keys };
+      }
+    };
+
     // Animation using Anime.js
     anime({
       targets: "#p",
       translateY: [-40, 0],
       opacity: [0, 1],
       easing: "easeInOutQuad",
-      duration: 500,
-      delay: 1000,
+      duration: 1000,
+      delay: 2000,
     });
     anime({
       targets: "#a",
       translateY: [40, 0],
       opacity: [0, 1],
       easing: "easeInOutQuad",
-      duration: 500,
-      delay: 1000,
+      duration: 1000,
+      delay: 2000,
     });
     anime({
       targets: "#n",
       translateY: [-40, 0],
       opacity: [0, 1],
       easing: "easeInOutQuad",
-      duration: 500,
-      delay: 1000,
+      duration: 1000,
+      delay: 2000,
     });
     anime({
       targets: "#k",
       translateY: [40, 0],
       opacity: [0, 1],
       easing: "easeInOutQuad",
-      duration: 500,
-      delay: 1000,
+      duration: 1000,
+      delay: 2000,
     });
     anime({
       targets: "#o",
       translateY: [-40, 0],
       opacity: [0, 1],
       easing: "easeInOutQuad",
-      duration: 500,
-      delay: 1000,
+      duration: 1000,
+      delay: 2000,
     });
-  }, []);
 
-  const getRandomWord = () => {
-    const wordArrayLen = words.length;
-    const wordSelection = words[Math.round(Math.random() * wordArrayLen)];
-    const ws = wordSelection.split(" ");
-    return {
-      p: ws[0],
-      a: ws[1],
-      n: ws[2],
-      k: ws[3],
-      o: ws[4],
-    };
-  };
+    const { letters, keyMap } = getRandomWord() || { letters: [], keyMap: [] };
+    setLetters(letters as []);
+    setKeyMap(keyMap as []);
+  }, [shuffles]);
 
-  const letters = getRandomWord();
+  if (!letters) return null;
 
   return (
     <div className="flex container mx-auto">
-      <div id="p" className="border rounded rounded-md p-2 me-2">
-        {letters["p"]}
-      </div>
-      <div id="a" className="border rounded rounded-md p-2 me-2">
-        {letters["a"]}
-      </div>
-      <div id="n" className="border rounded rounded-md p-2 me-2">
-        {letters["n"]}
-      </div>
-      <div id="k" className="border rounded rounded-md p-2 me-2">
-        {letters["k"]}
-      </div>
-      <div id="o" className="border rounded rounded-md p-2 me-2">
-        {letters["o"]}
-      </div>
+      {letters?.map((word, i) => {
+        return (
+          <div
+            key={keyMap[i]}
+            id={keyMap[i]}
+            className="border rounded-md p-2 me-2 text-emerald-900"
+          >
+            {word}
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        className="text-emerald-900"
+        onClick={() => setShuffles(shuffles + 1)}
+      >
+        Shuffle
+      </button>
     </div>
   );
 }
