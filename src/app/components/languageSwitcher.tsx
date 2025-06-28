558ab7158118e2b6f6f7c141e8a127b313af2fee
Steven Smith <stesmi@gmail.com>
2025-06-28 13:12:57 +0200

    Contact form nav

--- src/app/components/languageSwitcher.tsx
+++ src/app/components/languageSwitcher.tsx
@@ -64,7 +64,7 @@ export default function LanguageSwitcher({ font }: { font: any }) {
       </li>
       <li className="flex me-3 items-center h-100">
         <Link
-          className={`${font.className} text-gray-800 text-sm ${
+          className={`${font.className} inline-flex items-center px-4 py-2 bg-gradient-to-r text-xs from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 rounded-lg text-white font-semibold transition-all transform hover:scale-105 shadow-lg ${
             decodeURI(pathname).includes("/folio/all") ? "underline" : ""
           }`}
           href="/10x/#contact"
